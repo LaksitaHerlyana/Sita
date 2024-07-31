db032dc31fcb766ede184de4f4f6e2b30acf5b71
LaksitaHerlyana <laksita_purnomo_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:56:14 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class Tes {
+    public static void main (String[] args) {
+    System.out.println("ini file Tes");
+    System.out.println("Halo");
+    System.out.println("Nama:Laksita Herlyana Purnomo"); 
+    System.out.println("Alamat:Jl Rh Ismail rt 008 rw 003 Jakarta Timur kel jatinegara kec cakung");
+    System.out.println("SMPN 90 Jakarta");
+    System.out.println("085788830983");
